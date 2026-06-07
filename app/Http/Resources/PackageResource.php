766245e89f9fc71e6f3be526ea9d040f4aa2f62f
EslamTeleb1 <et1136@fayoum.edu.eg>
2026-06-08 00:21:25 +0300

    retrun title

--- app/Http/Resources/PackageResource.php
+++ app/Http/Resources/PackageResource.php
@@ -12,7 +12,12 @@ class PackageResource extends JsonResource
             'id' => $this->id,
             'service_id' => $this->service_id,
             'title' => $this->getTranslations('title'),
+            'title_en' => $this->getTranslation('title', 'en', false),
+            'title_ar' => $this->getTranslation('title', 'ar', false),
             'description' => $this->getTranslations('description'),
+            'description_en' => $this->getTranslation('description', 'en', false),
+            'description_ar' => $this->getTranslation('description', 'ar', false),
+
             'is_active' => $this->is_active,
             'service' => new ServiceResource($this->whenLoaded('service')),
             'created_at' => optional($this->created_at)->toDateTimeString(),
